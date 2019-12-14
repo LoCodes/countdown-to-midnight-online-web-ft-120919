7129691be3088ca4c915ff15b7531c542ba9ab48
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-14 07:56:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
-
+countdown = 0
 def countdown(seconds_to_midnight)
-  while number > 0
+  while seconds_to_midnight > 0
     puts "#{seconds_to_midnight} SECONDS(S)!"
-    number -= 1
+    seconds_to_midnight -= 1
   end
  puts "HAPPY NEW YEAR!"
 end
