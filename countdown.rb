6c983836c7f449b23a358cb257bb242c3451a1ed
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-14 07:52:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
+countdown = 0 
 
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECONDS(S)!"
+    number -= 1
+  end
+ puts "HAPPY NEW YEAR!"
+end
+  
+
+def countdown_with_sleep
+  puts "HAPPY NEW YEAR!"
 end
