8a30ea678a47ed38efef7d771dcea120294b1cf1
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-14 07:59:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,16 @@
-
 def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECONDS(S)!"
+    puts "#{seconds_to_midnight} SECOND(S)!"
     seconds_to_midnight -= 1
   end
-    puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
-  
 
-def countdown_with_sleep
-  puts "HAPPY NEW YEAR!"
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
